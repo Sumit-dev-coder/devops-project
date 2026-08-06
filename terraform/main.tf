e03a781e7ddd77c07c442fea8ac4869b57ecb6d6
Sumit-dev-coder <sumit.ai@outlook.com>
2026-08-06 11:12:30 +0530

    I modified the main.tf t3.micro

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,7 +41,7 @@ resource "aws_security_group" "app_sg" {
 
 resource "aws_instance" "app_server" {
   ami                    = "ami-0c02fb55956c7d316"
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.app_sg.id]
 
   tags = {
