b3fa1c017390f273b3d1505296191af856ee1d32
Sumit-dev-coder <sumit.ai@outlook.com>
2026-08-06 13:07:45 +0530

    I modified main.tf and add public key to take the ssh

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,9 +39,15 @@ resource "aws_security_group" "app_sg" {
   }
 }
 
+resource "aws_key_pair" "deployer" {
+  key_name   = "devops-project-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCbdwmMpmpaHlugd+lHxNJkKXI88JoNcd8Ru5ImfumG4Gy7OHuNVsyoWJgPMiqcLjli/cZ9L0JqjQNqmeOo16JOEf7Hfn9ZD9TFm+7FzRaqIsjTQwCIuIVPz7M0xcFzL8fMRxe4hAD6AEnSHlzvySA20hnvWAmGDHKkA2mg1YYczNu2QT1dfVID1thOfw9x9Tgrc/95VJjDfJ57lGWj3fe+lYcmB8Y1rmSKt9t4+xtUawJHFjZm6orslAp9CX9Vcphk7oxTWqqEypgBQO3MA2Qtz1Y87Ze2Vf0SEAanlfiqZtDdQJY0dYoMpIyOQPzK/G3P4ZtR6UQSu6slLyqPitonQQl7y4H304EERiodU4zK85vM320yHnjOWXO6zKhgqqJ/breNNFNlaRH9AFVZyc7F9l6TdMjQq1+TUlYBxvsrft+dZT5K7ward/onAZCMEQJ16X2GcxzgAl86kU3F6r1FGKGRWB3bUCA0LI2uQJ6elakzdMizoFi82QS6kYQSaZeA7vg2wFQLFs2iHpsWX0pv9anNQ5hPLYcCjyG21E56cP1L+Mn6Id4S8W/kXfslOrOaHoc3Xo5zqNonP2CKACDckVuwPZHICPyy4RixI1eSr/bV0BMz+sCtOUeT7UDu8l0T5+4+keNT0tzWzoefTtoyo2Az6/HftUnygBZEbQPC/Q== sumit@LHEA01311"
+}
+
 resource "aws_instance" "app_server" {
   ami                    = "ami-0c02fb55956c7d316"
   instance_type          = "t3.micro"
+  key_name               = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.app_sg.id]
 
   tags = {
